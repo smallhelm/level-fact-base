290c799561133bce9f962d9b7e64070bd63b8d50
Matthew Wright <matthew@smallhelm.com>
2015-06-06 07:47:52 -0600

    convenience fn to setup a transactor and get all the common fb fns

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-module.exports = require('./transactor');
+var inq = require('./inquisitor');
+var Transactor = require('./transactor');
+
+module.exports = function(db, options, onStartup){
+  if(arguments.length === 2){
+    onStartup = options;
+    options = {};
+  }
+  Transactor(db, options, function(err, transactor){
+    if(err) return onStartup(err);
+
+    onStartup(null, {
+      transact: transactor.transact,
+
+      connection: transactor.connection,
+      snap: transactor.connection.snap,
+      asOf: transactor.connection.asOf,
+
+      q: inq.q,
+      qTuple: inq.qTuple,
+      getEntity: inq.getEntity
+    });
+  });
+};
