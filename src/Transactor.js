da6e48f34e90df8785391a27afbce7e83c9f95b8
Matthew Wright <matthew@smallhelm.com>
2018-04-06 17:08:14 -0500

    starting schema support

--- src/Transactor.js
+++ src/Transactor.js
@@ -1,4 +1,6 @@
+var λ = require('contra')
 var cuid = require('cuid')
+var dbRange = require('./dbRange')
 var fastq = require('fastq')
 var promisify = require('./promisify')
 
@@ -75,8 +77,75 @@ function transact (db, txn, entities, callback) {
   })
 }
 
+var systemSchema = {
+  '_s/type': {
+    '_s/type': 'String'
+  },
+  '_s/txn-time': {
+    '_s/type': 'Date'
+  }
+}
+
+function parseFact (dbKey) {
+  var fact = {}
+  dbKey[0].split('').forEach(function (key, i) {
+    fact[key] = dbKey[i + 1]
+  })
+  return fact
+}
+
+function getEntity (db, txn, id, callback) {
+  var facts = []
+  dbRange(db, {
+    prefix: ['eavto', id]
+  }, function (data) {
+    if (data.key[4] > txn) {
+      return// too new
+    }
+    facts.push(parseFact(data.key))
+  }, function (err) {
+    if (err) return callback(err)
+    facts.sort(function (a, b) {
+      return a.t - b.t
+    })
+    var entity = {}
+    facts.forEach(function (fact) {
+      if (fact.o) {
+        entity[fact.a] = fact.v
+      } else {
+        delete entity[fact.a]
+      }
+    })
+    callback(null, entity)
+  })
+}
+
+function loadSchemaAsOf (db, txn, callback) {
+  var attrIds = []
+  dbRange(db, {
+    prefix: ['aveto', '_s/attr']
+  }, function (data) {
+    if (data.key[4] > txn) {
+      return// too new
+    }
+    attrIds.push(data.key[3])
+  }, function (err) {
+    if (err) return callback(err)
+
+    var schema = {}
+    λ.each(attrIds, function (id, next) {
+      getEntity(db, txn, id, function (err, entity) {
+        if (err) return next(err)
+        schema[id] = entity
+        next()
+      })
+    }, function (err) {
+      callback(err, schema)
+    })
+  })
+}
+
 function loadCurrFB (db, callback) {
-  var schema = {}// TODO
   db.get('latest-txn', function (err, txn) {
     if (err) {
       if (err.notFound) {
@@ -85,35 +154,15 @@ function loadCurrFB (db, callback) {
         return callback(err)
       }
     }
-    callback(null, mkFB(db, txn, schema))
+    loadSchemaAsOf(db, txn, function (err, schema) {
+      if (err) return callback(err)
+      callback(null, mkFB(db, txn, schema))
+    })
   })
 }
 
 module.exports = function Transactor (db) {
   var currFB
-
-  var onLoadCBs = []
-  var errorLoading = null
-
-  function onLoad (callback) {
-    if (errorLoading || currFB) {
-      callback(errorLoading, currFB)
-      return
-    }
-    onLoadCBs.push(callback)
-    if (onLoadCBs.length > 1) {
-      return
-    }
-
-    loadCurrFB(db, function (err, fb) {
-      errorLoading = err
-      currFB = fb
-
-      while (onLoadCBs.length > 0) {
-        onLoadCBs.shift()(errorLoading, currFB)
-      }
-    })
-  }
 
   var transactQ = fastq(function (entities, callback) {
     var txn = currFB.txn + 1
@@ -125,6 +174,73 @@ module.exports = function Transactor (db) {
       callback(null, currFB)
     })
   })
+
+  function syncSchema (callback) {
+    var currSchema = currFB.schema
+    var goalSchema = systemSchema
+
+    var toTransact = []
+
+    Object.keys(currSchema).forEach(function (attrId) {
+      if (!goalSchema[attrId]) {
+        // remove the old val
+        toTransact.push(Object.assign({}, currSchema[attrId], {
+          $e: attrId,
+          $retract: true
+        }))
+      }
+    })
+
+    Object.keys(goalSchema).forEach(function (attrId) {
+      var entity = {
+        $e: attrId,
+        '_s/attr': attrId
+      }
+      var foundDiff = false
+      Object.keys(goalSchema[attrId]).forEach(function (attr) {
+        if (attr === '_s/attr') return
+        var goalV = goalSchema[attrId][attr]
+        var currV = currSchema[attrId] && currSchema[attrId][attr]
+        if (goalV !== currV) {
+          entity[attr] = goalSchema[attrId][attr]
+          foundDiff = true
+        }
+      })
+      if (foundDiff) {
+        toTransact.push(entity)
+      }
+    })
+    transactQ.push(toTransact, callback)
+  }
+
+  var onLoadCBs = []
+  var doneLoading = false
+  var errorLoading = null
+
+  var callOnLoadCBs = function (err) {
+    errorLoading = err
+    doneLoading = true
+    while (onLoadCBs.length > 0) {
+      onLoadCBs.shift()(errorLoading, currFB)
+    }
+  }
+
+  function onLoad (callback) {
+    if (errorLoading || doneLoading) {
+      callback(errorLoading, currFB)
+      return
+    }
+    onLoadCBs.push(callback)
+    if (onLoadCBs.length > 1) {
+      return
+    }
+
+    loadCurrFB(db, function (err, fb) {
+      if (err) return callOnLoadCBs(err)
+      currFB = fb
+      syncSchema(callOnLoadCBs)
+    })
+  }
 
   return {
     snap: function (callback) {
--- src/tests.js
+++ src/tests.js
@@ -13,17 +13,52 @@ function mkDB () {
   }))
 }
 
+async function dbDump (fb, keepSchema) {
+  var dbStr = ''
+  await dbRange(fb.db, {
+    prefix: []
+  }, function (data, stopRange) {
+    if (!keepSchema) {
+      if (/^_s\//.test(data.key[data.key[0].indexOf('a') + 1])) {
+        return
+      }
+    }
+    dbStr += data.key.join('|') + '\n'
+  })
+  return dbStr.trim()
+}
+
 test('Transactor', async function (t) {
   var db = mkDB()
   var tr = Transactor(db)
+  var fb
+  fb = await tr.snap()
+  t.is(fb.txn, 1, 'schema transacted')
+  t.is(await dbDump(fb, true), `
+aveto|_s/attr|_s/txn-time|_s/txn-time|1|true
+aveto|_s/attr|_s/type|_s/type|1|true
+aveto|_s/type|Date|_s/txn-time|1|true
+aveto|_s/type|String|_s/type|1|true
+eavto|_s/txn-time|_s/attr|_s/txn-time|1|true
+eavto|_s/txn-time|_s/type|Date|1|true
+eavto|_s/type|_s/attr|_s/type|1|true
+eavto|_s/type|_s/type|String|1|true
+teavo|1|_s/txn-time|_s/attr|_s/txn-time|true
+teavo|1|_s/txn-time|_s/type|Date|true
+teavo|1|_s/type|_s/attr|_s/type|true
+teavo|1|_s/type|_s/type|String|true
+vaeto|Date|_s/type|_s/txn-time|1|true
+vaeto|String|_s/type|_s/type|1|true
+vaeto|_s/txn-time|_s/attr|_s/txn-time|1|true
+vaeto|_s/type|_s/attr|_s/type|1|true
+`.trim())
 
   var error = await t.throws(tr.transact([{name: 'bob'}]))
   t.is(error + '', 'Error: Fact tuple missing `$e`')
 
-  var fb
   fb = await tr.transact([])
   fb = await tr.transact([{$e: 'A0'}])
-  t.is(fb.txn, 0, 'nothing actually transacted')
+  t.is(fb.txn, 1, 'nothing actually transacted')
 
   fb = await tr.transact([
     {
@@ -36,41 +71,35 @@ test('Transactor', async function (t) {
       foo: 'bar'
     }
   ])
-  t.is(fb.txn, 1, 'txn 1 finished')
+  t.is(fb.txn, 2, 'txn 2 finished')
 
   fb = await tr.transact([{
     $e: 'BB',
     foo: 'baz'
   }])
-  t.is(fb.txn, 2, 'txn 2 finished')
+  t.is(fb.txn, 3, 'txn 3 finished')
 
-  var dbStr = ''
-  await dbRange(fb.db, {
-    prefix: []
-  }, function (data, stopRange) {
-    dbStr += data.key.join('|') + '\n'
-  })
-  t.is(dbStr.trim(), `
-aveto|email|some@email|AA|1|true
-aveto|foo|bar|BB|1|true
-aveto|foo|baz|BB|2|true
-aveto|name|bob|AA|1|true
-eavto|AA|email|some@email|1|true
-eavto|AA|name|bob|1|true
-eavto|BB|foo|bar|1|true
-eavto|BB|foo|baz|2|true
-teavo|1|AA|email|some@email|true
-teavo|1|AA|name|bob|true
-teavo|1|BB|foo|bar|true
-teavo|2|BB|foo|baz|true
-vaeto|bar|foo|BB|1|true
-vaeto|baz|foo|BB|2|true
-vaeto|bob|name|AA|1|true
-vaeto|some@email|email|AA|1|true
+  t.is(await dbDump(fb), `
+aveto|email|some@email|AA|2|true
+aveto|foo|bar|BB|2|true
+aveto|foo|baz|BB|3|true
+aveto|name|bob|AA|2|true
+eavto|AA|email|some@email|2|true
+eavto|AA|name|bob|2|true
+eavto|BB|foo|bar|2|true
+eavto|BB|foo|baz|3|true
+teavo|2|AA|email|some@email|true
+teavo|2|AA|name|bob|true
+teavo|2|BB|foo|bar|true
+teavo|3|BB|foo|baz|true
+vaeto|bar|foo|BB|2|true
+vaeto|baz|foo|BB|3|true
+vaeto|bob|name|AA|2|true
+vaeto|some@email|email|AA|2|true
   `.trim())
 
   // Try a cold start
   var tr2 = Transactor(db)
   fb = await tr2.snap()
-  t.is(fb.txn, 2, 'loaded the txn')
+  t.is(fb.txn, 3, 'loaded the txn')
 })
